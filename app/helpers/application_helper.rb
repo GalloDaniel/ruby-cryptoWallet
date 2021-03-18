35d329ad461b80b0a85841c2a3a7884274793845
GalloDaniel <danielcostagallo@gmail.com>
2021-03-18 18:43:58 -0300

    first commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,19 @@
+module ApplicationHelper
+  def locale
+    I18n.locale == :en ? "Inglês Americano" : "Português do Brasil"
+  end
+
+  def date_br(date_us)
+    date_us.strftime('%d/%m/%Y')        
+  end  
+
+  def ambiente_rails
+    if Rails.env.development?
+      "Desenvolvimento"
+    elsif Rails.env.production?
+      "Produção"
+    else
+      "Teste"
+    end    
+  end
+end
